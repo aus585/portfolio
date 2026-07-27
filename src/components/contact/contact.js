b77bfe7549d9f71d903fbd214f01f98fcbbea31b
Aryan Shirke <aaryana1u5s.shirke@gmail.com>
2026-07-27 08:38:17 +0530

    Add copyright line to contact footer
    Added a copyright line to the footer with the current year.

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -193,6 +193,12 @@ const json = JSON.stringify(object);
             <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"/></svg>
             <span>Your information will never be shared.</span>
           </div>
+
+          <!-- ADDED COPYRIGHT LINE -->
+          <div class="copyright-footer">
+            <span>&copy; ${new Date().getFullYear()} Aryan Shirke. All rights reserved.</span>
+          </div>
+
         </div>
 
       </div>
